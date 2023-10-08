95af710a5d573f5729cbce9eed3537409afa1c06
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-08 22:45:15 +0400

    Create practice.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice.py
@@ -0,0 +1,44 @@
+'''Name = input (" Enter your name : ")
+print (Name)'''
+
+Name = (input (" Enter your name : "))
+    
+Age = (input (" Enter your Age : "))
+
+Telephone = (input (" Enter your Telephone : "))
+
+Address = (input (" Enter your Address : "))
+
+Father_name = (input (" Enter your father's name : "))
+
+Mother_name = (input (" Enter your mother's name : "))
+
+Occupation = (input (" Enter your occupation : "))
+
+Marital_Status = (input (" Enter your marital status : "))
+
+Pet_name = (input (" Enter your pet's name : "))
+
+Color = (input (" Enter your favorite color : "))
+
+
+
+print(Name)
+
+print(Age)
+
+print(Telephone)
+
+print(Address)
+
+print(Father_name)
+
+print(Mother_name)
+
+print(Occupation)
+
+print(Marital_Status)
+
+print(Pet_name)
+
+print(Color)
