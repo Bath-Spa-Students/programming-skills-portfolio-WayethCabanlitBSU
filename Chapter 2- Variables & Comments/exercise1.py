ddc5014db81559768d3cd804324293b6d1dfa959
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-13 15:48:44 +0400

    Create exercise1.py

---
+++ Chapter 2- Variables & Comments/exercise1.py
@@ -0,0 +1,8 @@
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+message="Welcome to the pentagon"
+print(message)
+
+message= "How is your day?"
+print(message)
+
