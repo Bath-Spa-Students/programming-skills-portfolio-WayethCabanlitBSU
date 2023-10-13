6beb6a21ca117ab04b3596c6d37bb877e8ef2b15
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-13 15:49:02 +0400

    Create exercise2.py

---
+++ Chapter 2- Variables & Comments/exercise2.py
@@ -0,0 +1,7 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+
+Author= "Mahatma Gandhi"
+message="You must be the change you wish to see in the world."
+
+print(f'{Author} once said "{message}"')
+
