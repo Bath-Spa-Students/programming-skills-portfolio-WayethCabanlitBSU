5967866e67c63a6c2f0031f489db84ea164893f4
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-14 17:32:06 +0400

    Create exercise3.py

---
+++ Chapter 2- Variables & Comments/exercise3.py
@@ -0,0 +1,7 @@
+#print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().
+name="\nWayeth Cabanlit\t"
+
+print (name)
+print (name.lstrip())
+print (name.rstrip())
+print (name.strip())
