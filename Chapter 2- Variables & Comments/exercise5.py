59ad951547ab943fb889079c377b26cc7f25c977
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-14 17:32:29 +0400

    Create exercise5.py

---
+++ Chapter 2- Variables & Comments/exercise5.py
@@ -0,0 +1,11 @@
+#Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
+
+import math
+
+Usb_bought= " Usbs bought = "
+Money_remaining= " Money remaining = "
+
+print(Usb_bought + str(int(50/6)))
+print(Money_remaining + str(50%6))
+
+
