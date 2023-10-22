4805cbb29b517e274cb46b8cf24130cbc28dcfbc
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-22 12:24:44 +0400

    Create exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,15 @@
+# Make a list that includes at least three people you’d like to invite to dinner. 
+
+guests_list = ['Rafael', 'Yzach', 'Paul', 'Marc']
+
+name = guests_list[0].title()
+print(f"{name}, come to my house and eat dinner at 9pm tonight.")
+
+name = guests_list[1].title()
+print(f"{name}, come to my house and eat dinner at 9pm tonight.")
+
+name = guests_list[2].title()
+print(f"{name}, come to my house and eat dinner at 9pm tonight.")
+
+name = guests_list[3].title()
+print(f"{name}, come to my house and eat dinner at 9pm tonight.")
