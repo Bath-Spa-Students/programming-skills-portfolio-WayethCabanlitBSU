baf5536a5c4ddb43916a3fc4c60754c512e9e93e
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-22 12:23:56 +0400

    Create exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,16 @@
+#Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them. 
+
+friends = ['Rafael', 'Yzach', 'Yyan','Paul']
+
+message= f"Hello {friends[0]}, Good morning!"
+print (message)
+
+message=f"Hello {friends[1]}, Good afternoon!"
+print (message)
+
+message= f"Hello {friends[2]}, Good evening!"
+print (message)
+
+message=f"Hello, {friends[3]}!"
+print (message)
+
