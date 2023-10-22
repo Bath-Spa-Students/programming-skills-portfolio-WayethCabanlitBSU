1ea32ea63b763a2113dd2cbd6d580f75f6d87155
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-22 16:48:06 +0400

    Create exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise7.py
@@ -0,0 +1,34 @@
+#Think of at least five places in the world you’d like to visit.
+
+Tourist_Destinations = ['Venice, Italy', 'Mt.Fuji, Japan', 'Palawan El Nido, Philippines', 'Niagra Falls, Canada', 'Zhangjiajie, China']
+
+print("Original order:")
+print(Tourist_Destinations)
+
+print("\nAlphabetical:")
+print(sorted(Tourist_Destinations))
+
+print("\nOriginal order:")
+print(Tourist_Destinations)
+
+print("\nReverse alphabetical:")
+print(sorted(Tourist_Destinations, reverse=True))
+
+print("\nOriginal order:")
+print(Tourist_Destinations )
+
+print("\nReversed:")
+Tourist_Destinations.reverse()
+print(Tourist_Destinations)
+
+print("\nOriginal order:")
+Tourist_Destinations.reverse()
+print(Tourist_Destinations)
+
+print("\nAlphabetical")
+Tourist_Destinations .sort()
+print(Tourist_Destinations)
+
+print("\nReverse alphabetical")
+Tourist_Destinations.sort(reverse=True)
+print(Tourist_Destinations)
