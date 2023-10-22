af26bb777979c44d95e62573fa07c57c911eb31f
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-22 12:24:00 +0400

    Create exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,14 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
+
+Transportation = ['Ferrari car', '2021 YZ 85 Dirt bike', 'Powell Peralta Skateboard']
+
+message= f"I would like to own a, {Transportation[0]} and use it everyday."
+print (message)
+
+message=f"I would like to own a, {Transportation[1]} and use it in a derby."
+print (message)
+
+message= f"I would like to own a, {Transportation[2]} and use it."
+print (message)
+
+
