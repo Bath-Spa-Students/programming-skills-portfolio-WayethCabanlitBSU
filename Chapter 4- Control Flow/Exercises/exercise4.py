1ecddbe00ffe3f4e08eeb0002f37abb72ddce5c2
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-29 20:47:09 +0400

    Create exercise4.py

---
+++ Chapter 4- Control Flow/Exercises/exercise4.py
@@ -0,0 +1,16 @@
+#Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+
+age = 69
+
+if age < 2:
+    print("You're a newborn baby!")
+elif age < 4:
+    print("You're still a developing toddler!")
+elif age < 12:
+    print("You're still considered a young kid!")
+elif age < 19:
+    print("You're in your adolescence!")
+elif age < 70:
+    print("You're an adult person!")
+else:
+    print("You're an old person!")
