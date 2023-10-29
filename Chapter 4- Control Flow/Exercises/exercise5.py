9ed8b12c2e6bc6f776f98958f1c35a8fad8c8b98
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-29 20:47:40 +0400

    Create exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,17 @@
+#Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+favorite_fruits_to_eat = ['Bananas', 'Tomatoes', 'Watermelons']
+
+if 'Watermelons' in favorite_fruits_to_eat:
+    print("Woah,You really like Watermelons!")
+
+if 'Lingonberries' in favorite_fruits_to_eat:
+    print("You really like Lingonberries!")
+
+if 'Bananas' in favorite_fruits_to_eat:
+    print("Wow!You must really like Bananas!")
+
+if 'Tomatoes' in favorite_fruits_to_eat:
+    print("Tomatoes are considered as a fruit and you really like Tomatoes!")
+
+if 'Jackfruit' in favorite_fruits_to_eat:
+    print("You really like Jackfruit!")
