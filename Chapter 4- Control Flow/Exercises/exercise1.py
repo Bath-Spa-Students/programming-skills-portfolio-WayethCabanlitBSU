24041bfa07d3222cef0dc7af4e8cef84e9eacbe4
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-29 20:40:58 +0400

    Create exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -0,0 +1,15 @@
+#Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+#Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+Color_of_Alien = 'Lime_Green'
+
+if Color_of_Alien== 'Lime_Green':
+    print("Congrats on winning 5 points!")
+
+#Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+    Color_of_Alien = 'Light_Blue'
+
+if Color_of_Alien == 'Lime_Green':
+    print("Congrats on winning 5 points!")
