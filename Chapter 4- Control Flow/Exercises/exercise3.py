f9a109ace696fc981f74f781f066a6eb6bc05564
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-10-29 20:46:15 +0400

    Create exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,46 @@
+#Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.
+ 
+#If the alien is yellow, print a message that the player earned 10 points.
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+
+    print("Congrats on winning 5 points!")
+
+elif alien_color == 'yellow':
+
+    print("Congrats on winning 10 points!")
+
+else:
+    print("Congrats on winning 15 points!")
+
+# If the alien is red, print a message that the player earned 15 points.
+
+alien_color = 'red'
+
+if alien_color == 'green':
+
+    print("Congrats on winning 5 points!")
+
+elif alien_color == 'yellow':
+
+    print("Congrats on winning 10 points!")
+
+else:
+    print("Congrats on winning 15 points!")
+
+#If the alien is green, print a message that the player earned 5 points.
+
+    alien_color = 'green'
+
+if alien_color == 'green':
+
+    print("Congrats on winning 5 points!")
+
+elif alien_color == 'yellow':
+
+    print("Congrats on winning 10 points!")
+
+else:
+    print("Congrats on winning 15 points!")
