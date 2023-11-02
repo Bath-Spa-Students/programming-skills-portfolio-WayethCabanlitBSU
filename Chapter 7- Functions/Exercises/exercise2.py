81dc5d739bf2b7f42606b4836d111fec5d44d8d1
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-11-02 09:33:48 +0400

    Create exercise2.py

---
+++ Chapter 7- Functions/Exercises/exercise2.py
@@ -0,0 +1,6 @@
+#Write a function called favorite_book() that accepts one parameter, title.
+
+def favorite_book(Book_title):
+    print(f"{Book_title} This is a book I like to read in my past time")
+
+favorite_book('Warhammer 40,000')
