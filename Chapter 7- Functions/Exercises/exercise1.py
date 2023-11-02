712ce7bf7464354b4911db83f703fa6e18648001
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-11-02 09:32:12 +0400

    Create exercise1.py

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,8 @@
+#Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter.
+
+def display_message():
+    Chapter_being_learned = "In this chapter I am learning about functions"
+
+    print(Chapter_being_learned)
+
+display_message()
