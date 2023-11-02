ed7991ffe297e995d4a0343a9f195adad1aec352
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-11-02 09:06:02 +0400

    Create exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -0,0 +1,24 @@
+Person_information= {
+    'first_name': 'Wayeth Ken',
+    'last_name': 'Cabanlit',
+    'age': 19,
+    'city': 'Ras Al Khaimah',
+    #Extra variables 
+    'Fathers name': 'Richard',
+    'Mothers name': 'Maria',
+    'Sibling name': 'Raven',
+    }
+
+print(Person_information['first_name'])
+
+print(Person_information['last_name'])
+
+print(Person_information['age'])
+
+print(Person_information['city'])
+
+print(Person_information['Fathers name'])
+
+print(Person_information['Mothers name'])
+
+print(Person_information['Sibling name'])
