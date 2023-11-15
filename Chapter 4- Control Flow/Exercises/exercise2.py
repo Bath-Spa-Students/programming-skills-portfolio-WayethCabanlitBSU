4488e765848d58ec4daf08574032534821d6384b
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-11-15 13:14:29 +0400

    Update exercise2.py

--- Chapter 4- Control Flow/Exercises/exercise2.py
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -10,9 +10,9 @@ else:
     print("You just earned 10 points!")
 
 #Write one version of this program that runs the if block and another that runs the else block.
-    Color_of_Alien = 'Lemon'
+    Color_of_Alien = 'Lime_green'
 
-if Color_of_Alien == 'Lime_green':
+if Color_of_Alien == 'Lemon':
     print("You shot the lime green alien, congrats on winning 5 points!")
 else:
     print("You shot the Lemon colored alien, congrats on winning 10 points!")
