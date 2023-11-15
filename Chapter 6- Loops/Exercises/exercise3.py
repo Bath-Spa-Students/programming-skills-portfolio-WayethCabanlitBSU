814a607520588fcb1b620d25b85f245de45e7724
WayethCabanlitBSU <cabanlit.wayethken2022@gmail.com>
2023-11-15 17:02:09 +0400

    Update exercise3.py

--- Chapter 6- Loops/Exercises/exercise3.py
+++ Chapter 6- Loops/Exercises/exercise3.py
@@ -1,8 +1,18 @@
-#Updated loop made using list
+#Loop code using list
 Start_loop = ["Loop"]
 
 while Start_loop:
     current_loop = Start_loop.pop()
     print(f"You are stuck in a {current_loop}, please wait a moment.")
     Start_loop.append(current_loop)
+
+#Input loop
+Input = "Enter any number to enter the loop: "
+while True:
+    Loop = input(Input)
+    
+    Loop= int(Loop)
+
+    if Loop < 10000000000000000:
+        print("You are stuck in a loop")
 
